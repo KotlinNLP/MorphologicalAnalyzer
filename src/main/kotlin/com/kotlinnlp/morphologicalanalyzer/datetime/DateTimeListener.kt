b54ebfd627ac2f51862e3b28981a7c94c5fe9c5c
KotlinNLP Authors <github@kotlinnlp.com>
2018-03-16 18:55:59 +0100

    Build DateTime with properties listening adding rules

--- src/main/kotlin/com/kotlinnlp/morphologicalanalyzer/datetime/DateTimeListener.kt
+++ src/main/kotlin/com/kotlinnlp/morphologicalanalyzer/datetime/DateTimeListener.kt
@@ -14,12 +14,18 @@ import com.kotlinnlp.neuraltokenizer.Token
 /**
  * An event listener for an ANTRL DateTime Parser.
  */
+@Suppress("FunctionName") // for Java overridden methods
 internal class DateTimeListener(private val tokens: List<Token>) : DateTimeBaseListener() {
 
   /**
    * The list of date-times recognized.
    */
   private val dateTimes: MutableList<DateTime> = mutableListOf()
+
+  /**
+   * The builder helper of a [DateTime].
+   */
+  private lateinit var dateTimeBuilder: DateTimeBuilder
 
   /**
    * @return the list of date-times recognized during the last parsing
@@ -29,7 +35,7 @@ internal class DateTimeListener(private val tokens: List<Token>) : DateTimeBaseL
   /**
    * The listener of the 'enter root' event.
    *
-   * @param ctx the context of the 'root' rule
+   * @param ctx the context of the 'root' rule that is being parsed
    */
   override fun enterRoot(ctx: DateTimeParser.RootContext?) {
 
@@ -37,17 +43,94 @@ internal class DateTimeListener(private val tokens: List<Token>) : DateTimeBaseL
   }
 
   /**
-   * The listener of the 'enter root' event.
+   * The listener of the 'enter datetime' event.
+   *
+   * @param ctx the context of the 'datetime' rule that is being parsed
+   */
+  override fun enterDatetime(ctx: DateTimeParser.DatetimeContext) {
+
+    this.dateTimeBuilder = DateTimeBuilder(this.tokens)
+  }
+
+  /**
+   * The listener of the 'exit datetime' event.
    *
    * @param ctx the context of the 'datetime' rule just parsed
    */
   override fun exitDatetime(ctx: DateTimeParser.DatetimeContext) {
 
     this.dateTimes.add(
-      DateTime(
-        startToken = this.tokens.indexOfFirst { it.startAt == ctx.start.startIndex },
-        endToken = this.tokens.indexOfFirst { it.endAt == ctx.stop.stopIndex }
-      )
+      this.dateTimeBuilder.getDateTime(startIndex = ctx.start.startIndex, endIndex = ctx.stop.stopIndex)
     )
+  }
+
+  /**
+   * The listener of the 'exit year' event.
+   *
+   * @param ctx the context of the 'year' rule just parsed
+   */
+  override fun exitYear(ctx: DateTimeParser.YearContext) {
+
+    this.dateTimeBuilder.setYearNum(ctx.start.startIndex)
+  }
+
+  /**
+   * The listener of the 'exit year_APEX' event.
+   *
+   * @param ctx the context of the 'year_APEX' rule just parsed
+   */
+  override fun exitYear_APEX(ctx: DateTimeParser.Year_APEXContext) {
+
+    this.dateTimeBuilder.setYearNum(ctx.start.startIndex)
+  }
+
+  /**
+   * The listener of the 'exit year_modern' event.
+   *
+   * @param ctx the context of the 'year_modern' rule just parsed
+   */
+  override fun exitYear_modern(ctx: DateTimeParser.Year_modernContext) {
+
+    this.dateTimeBuilder.setYearNum(ctx.start.startIndex)
+  }
+
+  /**
+   * The listener of the 'exit month_num' event.
+   *
+   * @param ctx the context of the 'month_num' rule just parsed
+   */
+  override fun exitMonth_num(ctx: DateTimeParser.Month_numContext) {
+
+    this.dateTimeBuilder.setMonthNum(ctx.start.startIndex)
+  }
+
+  /**
+   * The listener of the 'exit month_str' event.
+   *
+   * @param ctx the context of the 'month_str' rule just parsed
+   */
+  override fun exitMonth_str(ctx: DateTimeParser.Month_strContext) {
+
+    this.dateTimeBuilder.setMonthStr(ctx.start.startIndex)
+  }
+
+  /**
+   * The listener of the 'exit day_num' event.
+   *
+   * @param ctx the context of the 'day_num' rule just parsed
+   */
+  override fun exitDay_num(ctx: DateTimeParser.Day_numContext) {
+
+    this.dateTimeBuilder.setDayNum(ctx.start.startIndex)
+  }
+
+  /**
+   * The listener of the 'exit day_str' event.
+   *
+   * @param ctx the context of the 'day_str' rule just parsed
+   */
+  override fun exitDay_str(ctx: DateTimeParser.Day_strContext) {
+
+    this.dateTimeBuilder.setDayStr(ctx.start.startIndex)
   }
 }
