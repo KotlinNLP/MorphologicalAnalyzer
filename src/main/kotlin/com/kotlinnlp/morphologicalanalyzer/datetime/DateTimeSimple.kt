5e2d437278049525e9a1556b9555ac71b48a8eca
KotlinNLP Authors <github@kotlinnlp.com>
2018-03-19 14:35:39 +0100

    Make DateTimeSimple as composed by a Date and a Time

--- src/main/kotlin/com/kotlinnlp/morphologicalanalyzer/datetime/DateTimeSimple.kt
+++ src/main/kotlin/com/kotlinnlp/morphologicalanalyzer/datetime/DateTimeSimple.kt
@@ -10,63 +10,28 @@ package com.kotlinnlp.morphologicalanalyzer.datetime
 /**
  * A simple date-time object.
  *
- * @property startToken the index of the first token of the expression
- * @property endToken the index of the last token of the expression
- * @property day the number of the day in the range [1, 31] (can be null)
- * @property weekDay the number of the week day in the range [1, 7] (can be null)
- * @property month the number of the mont in the range [1, 12] (can be null)
- * @property year the number of the year in the range [0, 9999] (can be null)
- * @property yearAbbr whether the [year] is intended as abbreviated form (e.g. '98 stands for 1998)
- * @property sec the number of the sec in the range [0, 59] (can be null)
- * @property min the number of the min in the range [0, 59] (can be null)
- * @property hour the number of the hour in the range [0, 23] (can be null)
+ * @property startToken the index of the first token of this expression
+ * @property endToken the index of the last token of this expression
+ * @property date the date that compose this date-time
+ * @property time the time that compose this date-time
  */
 data class DateTimeSimple(
-  val startToken: Int,
-  val endToken: Int,
-  val day: Int?,
-  val weekDay: Int?,
-  val month: Int?,
-  val year: Int?,
-  val yearAbbr: Boolean,
-  val sec: Int?,
-  val min: Int?,
-  val hour: Int?
+  override val startToken: Int,
+  override val endToken: Int,
+  val date: Date,
+  val time: Time
 ) : DateTime {
 
-  companion object {
-
-    /**
-     * The list of week day names.
-     */
-    val WEEK_DAYS = listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
-  }
-
   /**
-   * Get the string representing this date-time in the following standard format: DD/MM/YYYY.
+   * Get the string representing this date-time in the following standard formats:
+   *   DD/MM/YYYY hh:mm:ss
    *
    * @return the string representing this date-time
    */
-  fun toStandardFormat(): String = "%s/%s/%s %s:%s:%s".format(
-    this.day?.let { "%02d".format(it) } ?: "-",
-    this.month?.let { "%02d".format(it) } ?: "-",
-    this.year?.let { (if (this.yearAbbr) "%02d" else "%04d").format(it) } ?: "-",
-    this.hour?.let { "%02d".format(it) } ?: "-",
-    this.min?.let { "%02d".format(it) } ?: "-",
-    this.sec?.let { "%02d".format(it) } ?: "-"
-  )
-
-  /**
-   * @return the standard string representation of the [weekDay]
-   */
-  fun weekDayToString(): String = this.weekDay?.let { WEEK_DAYS[it - 1] } ?: "-"
+  override fun toStandardFormat(): String = "%s %s".format(this.date.toStandardFormat(), this.time.toStandardFormat())
 
   /**
    * @return a string representation of this date-time object
    */
-  override fun toString(): String = "%s (week day: %s, year abbr.: %s)".format(
-    this.toStandardFormat(),
-    this.weekDayToString(),
-    this.yearAbbr
-  )
+  override fun toString(): String = "%s %s".format(this.date, this.time)
 }
