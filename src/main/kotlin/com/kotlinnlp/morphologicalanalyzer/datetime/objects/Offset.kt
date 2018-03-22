6e55784ea06de2b318c88bc64fda2cdefee608c7
KotlinNLP Authors <github@kotlinnlp.com>
2018-03-22 18:39:54 +0100

    Convert sub-classes to data classes

--- src/main/kotlin/com/kotlinnlp/morphologicalanalyzer/datetime/objects/Offset.kt
+++ src/main/kotlin/com/kotlinnlp/morphologicalanalyzer/datetime/objects/Offset.kt
@@ -12,30 +12,23 @@ import com.kotlinnlp.morphologicalanalyzer.datetime.utils.TimeObj
 
 /**
  * An offset object.
- *
- * @property startToken the index of the first token of this expression
- * @property endToken the index of the last token of this expression
- * @property positive whether this offset is positive or negative
- * @property units the count of offset units, in the range [0, +inf] (e.g. + 2 weeks)
  */
-sealed class Offset(
-  override val startToken: Int,
-  override val endToken: Int,
-  val positive: Boolean,
-  val units: Int
-) : DateTime {
+sealed class Offset : DateTime {
+
+  /**
+   * Whether this offset is positive or negative.
+   */
+  abstract val positive: Boolean
+
+  /**
+   * The count of offset units, in the range [0, +inf] (e.g. + 2 weeks).
+   */
+  abstract val units: Int
 
   /**
    * The string prefix: "(+|-) UNITS".
    */
   protected val stringPrefix: String by lazy { "%s %d".format(if (this.positive) "+" else "-", this.units) }
-
-  /**
-   * Check requirements.
-   */
-  init {
-    require(this.units >= 0) { "Units must be >= 0." }
-  }
 
   /**
    * Get the string representing this offset in the following standard format:
@@ -53,10 +46,19 @@ sealed class Offset(
   /**
    * An offset of [DateObj].
    *
+   * @property startToken the index of the first token of this expression
+   * @property endToken the index of the last token of this expression
+   * @property positive whether this offset is positive or negative
+   * @property units the count of offset units, in the range [0, +inf] (e.g. + 2 weeks)
    * @property value the Date value
    */
-  class Date(startToken: Int, endToken: Int, positive: Boolean, units: Int, val value: DateObj)
-    : Offset(startToken = startToken, endToken = endToken, positive = positive, units = units) {
+  data class Date(
+    override val startToken: Int,
+    override val endToken: Int,
+    override val positive: Boolean,
+    override val units: Int,
+    val value: DateObj
+  ) : Offset() {
 
     /**
      * Get the string representing this offset in the following standard format:
@@ -72,8 +74,13 @@ sealed class Offset(
    *
    * @property value the Time value
    */
-  class Time(startToken: Int, endToken: Int, positive: Boolean, units: Int, val value: TimeObj)
-    : Offset(startToken = startToken, endToken = endToken, positive = positive, units = units) {
+  class Time(
+    override val startToken: Int,
+    override val endToken: Int,
+    override val positive: Boolean,
+    override val units: Int,
+    val value: TimeObj
+  ) : Offset() {
 
     /**
      * Get the string representing this offset in the following standard format:
@@ -87,48 +94,80 @@ sealed class Offset(
   /**
    * An offset of hours.
    */
-  class Hours(startToken: Int, endToken: Int, positive: Boolean, units: Int)
-    : Offset(startToken = startToken, endToken = endToken, positive = positive, units = units)
+  class Hours(
+    override val startToken: Int,
+    override val endToken: Int,
+    override val positive: Boolean,
+    override val units: Int
+  ) : Offset()
 
   /**
    * An offset of minutes.
    */
-  class Minutes(startToken: Int, endToken: Int, positive: Boolean, units: Int)
-    : Offset(startToken = startToken, endToken = endToken, positive = positive, units = units)
+  class Minutes(
+    override val startToken: Int,
+    override val endToken: Int,
+    override val positive: Boolean,
+    override val units: Int
+  ) : Offset()
 
   /**
    * An offset of seconds.
    */
-  class Seconds(startToken: Int, endToken: Int, positive: Boolean, units: Int)
-    : Offset(startToken = startToken, endToken = endToken, positive = positive, units = units)
+  class Seconds(
+    override val startToken: Int,
+    override val endToken: Int,
+    override val positive: Boolean,
+    override val units: Int
+  ) : Offset()
 
   /**
    * An offset of days.
    */
-  class Days(startToken: Int, endToken: Int, positive: Boolean, units: Int)
-    : Offset(startToken = startToken, endToken = endToken, positive = positive, units = units)
+  class Days(
+    override val startToken: Int,
+    override val endToken: Int,
+    override val positive: Boolean,
+    override val units: Int
+  ) : Offset()
 
   /**
    * An offset of weeks.
    */
-  class Weeks(startToken: Int, endToken: Int, positive: Boolean, units: Int)
-    : Offset(startToken = startToken, endToken = endToken, positive = positive, units = units)
+  class Weeks(
+    override val startToken: Int,
+    override val endToken: Int,
+    override val positive: Boolean,
+    override val units: Int
+  ) : Offset()
 
   /**
    * An offset of weekends.
    */
-  class Weekends(startToken: Int, endToken: Int, positive: Boolean, units: Int)
-    : Offset(startToken = startToken, endToken = endToken, positive = positive, units = units)
+  class Weekends(
+    override val startToken: Int,
+    override val endToken: Int,
+    override val positive: Boolean,
+    override val units: Int
+  ) : Offset()
 
   /**
    * An offset of months.
    */
-  class Months(startToken: Int, endToken: Int, positive: Boolean, units: Int)
-    : Offset(startToken = startToken, endToken = endToken, positive = positive, units = units)
+  class Months(
+    override val startToken: Int,
+    override val endToken: Int,
+    override val positive: Boolean,
+    override val units: Int
+  ) : Offset()
 
   /**
    * An offset of years.
    */
-  class Years(startToken: Int, endToken: Int, positive: Boolean, units: Int)
-    : Offset(startToken = startToken, endToken = endToken, positive = positive, units = units)
+  class Years(
+    override val startToken: Int,
+    override val endToken: Int,
+    override val positive: Boolean,
+    override val units: Int
+  ) : Offset()
 }
