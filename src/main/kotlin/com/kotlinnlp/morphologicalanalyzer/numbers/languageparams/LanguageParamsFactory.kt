c4c836919c841ef2a0c01f2a78ed293e45f69e6f
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-27 17:06:27 +0200

    Make LanguageParamsFactory internal

--- src/main/kotlin/com/kotlinnlp/morphologicalanalyzer/numbers/languageparams/LanguageParamsFactory.kt
+++ src/main/kotlin/com/kotlinnlp/morphologicalanalyzer/numbers/languageparams/LanguageParamsFactory.kt
@@ -14,7 +14,7 @@ import java.nio.file.Paths
 /**
  * The [LanguageParams] factory.
  */
-object LanguageParamsFactory {
+internal object LanguageParamsFactory {
 
   /**
    * Create a [LanguageParams] object.
