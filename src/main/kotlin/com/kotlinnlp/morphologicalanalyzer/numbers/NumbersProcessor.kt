0b53758166db3e8c42cd3e1d57006889c7bd94ed
KotlinNLP Authors <github@kotlinnlp.com>
2018-08-01 02:31:16 +0200

    Enabled fallback to LL parsing strategy when SLL fails

--- src/main/kotlin/com/kotlinnlp/morphologicalanalyzer/numbers/NumbersProcessor.kt
+++ src/main/kotlin/com/kotlinnlp/morphologicalanalyzer/numbers/NumbersProcessor.kt
@@ -22,6 +22,9 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker
 import kotlin.reflect.KClass
 import kotlin.reflect.full.primaryConstructor
 import org.antlr.v4.runtime.BailErrorStrategy
+import org.antlr.v4.runtime.DefaultErrorStrategy
+import org.antlr.v4.runtime.ConsoleErrorListener
+import org.antlr.v4.runtime.RecognitionException
 import org.antlr.v4.runtime.misc.ParseCancellationException
 
 
@@ -62,30 +65,29 @@ class NumbersProcessor(
    *
    * @return the list of number tokens found
    */
-  fun findNumbers(text: String, tokens: List<RealToken>, SLL: Boolean = true, fb: Boolean = true): List<Number> {
-
-    var fallback = fb
-    if (! SLL) fallback = false
+  fun findNumbers(text: String, tokens: List<RealToken>): List<Number> {
 
     return if (text.trim().isNotEmpty()) {
 
       val listener: ListenerCommon = this.buildListener(tokens)
 
-      val root = try{
+      val (parser, root) = try{
 
-        debugPrint("Executing with${if (SLL) "" else "out"} SLL")
-        this.buildParseTree(text, SLL = SLL)
-
+        this.buildParseTree(text, true)
       } catch (ex: ParseCancellationException) {
 
-        if(fallback) {
-          debugPrint("Executing fallback LL")
-          this.buildParseTree(text, SLL = false)
-        }
-        else throw ex
+        this.buildParseTree(text, false)
       }
 
-      ParseTreeWalker().walk(listener as ParseTreeListener, root)
+          debugPrint("Executing fallback LL")
+        ParseTreeWalker().walk(listener as ParseTreeListener, root)
+      } catch (ex: ParseCancellationException) {
+
+        //parser.addErrorListener(ConsoleErrorListener.INSTANCE)
+        parser.errorHandler = DefaultErrorStrategy()
+        parser.interpreter.predictionMode = PredictionMode.LL
+        ParseTreeWalker().walk(listener as ParseTreeListener, root)
+      }
 
       listener.getNumbers()
 
@@ -121,17 +123,19 @@ class NumbersProcessor(
    *
    * @return the parse tree of the given tokens stream
    */
-  private fun buildParseTree(text: String, SLL: Boolean): ParserRuleContext {
+  private fun buildParseTree(text: String, SLL: Boolean): Pair<Parser, ParserRuleContext> {
 
     val lexer: Lexer = this.buildLexer(charStream = CharStreams.fromString(text))
     val tokensStream = CommonTokenStream(lexer)
 
     return when (this.langParams.language) {
       "en" -> {
-        NumbersENParser(tokensStream).apply { if(SLL) this.enableSLL() }.root()
+        val p = NumbersENParser(tokensStream).apply { if(SLL) this.enableSLL() }
+        Pair(p, p.root())
       }
       "it" -> {
-        NumbersITParser(tokensStream).apply { if(SLL) this.enableSLL() }.root()
+        val p = NumbersITParser(tokensStream).apply { if(SLL) this.enableSLL() }
+        Pair(p, p.root())
     }
       else -> throw RuntimeException("Parser not available for language '${this.langParams.language}'")
     }
